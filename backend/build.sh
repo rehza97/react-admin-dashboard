8d6d2485700f3d17c820c6ac1358c7ba7a43889c
fethallah <fethotebib3@gmail.com>
2025-03-15 06:56:35 +0100

    Fix superuser creation condition in build script
    - Updated the condition in build.sh to check if CREATE_SUPERUSER is set to "True" for creating a superuser, ensuring proper functionality.

--- backend/build.sh
+++ backend/build.sh
@@ -6,7 +6,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; 
+if [[ $CREATE_SUPERUSER == "True" ]]; 
 then
     python manage.py createsuperuser --no-input
     echo "Superuser created"
