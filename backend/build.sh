3cb50ba88ef1b25b28670cfe7ab4c63e57391c6e
fethallah <fethotebib3@gmail.com>
2025-03-15 05:15:40 +0100

    Add superuser creation option in build script
    - Updated build.sh to include a conditional for creating a superuser based on the CREATE_SUPERUSER environment variable.
    - Added echo statements to indicate whether a superuser was created or not.

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,10 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
+if [[ $CREATE_SUPERUSER ]]; 
+then
+    python manage.py createsuperuser --no-input
+    echo "Superuser created"
+else
+    echo "Superuser not created"
+fi
